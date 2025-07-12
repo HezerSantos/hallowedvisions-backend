2ac56caa200fa7e1a6efe7d62f3b437acef0ee9a
HezerSantos <HezerSantos24@gmail.com>
2025-07-12 01:55:18 -0500

    modifed and prepping for csrf prod

--- src/controllers/csrf/getCsrf.ts
+++ src/controllers/csrf/getCsrf.ts
@@ -11,7 +11,6 @@ const getCsrfController: RequestHandler = async(req, res, next) => {
         const csrfValue = crypto.randomBytes(32).toString('hex')
 
         const csrfToken:string = jwt.sign({csrfToken: csrfValue}, String(process.env.CSRF_SECRET), {expiresIn: '15m'})
-        console.log(req.cookies)
         res.cookie('__Secure-auth.csrf', csrfToken, {
             httpOnly: false, 
             secure: true, 
@@ -20,6 +19,7 @@ const getCsrfController: RequestHandler = async(req, res, next) => {
             path: "/",
             domain: process.env.NODE_ENV === "production"? ".hallowedvisions.com" : ""
         })
+
 
         res.status(200).end()
     } catch(error) {
