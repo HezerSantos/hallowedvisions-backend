e35c0909ba504fb860f1df78540857095c0dff80
HezerSantos <HezerSantos24@gmail.com>
2025-07-13 03:11:58 -0500

    Created the endpoint for retrieving R2 home object

--- src/controllers/home/GET/getHomeAssets.ts
+++ src/controllers/home/GET/getHomeAssets.ts
@@ -1,8 +1,31 @@
 import { RequestHandler } from "express";
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import dotenv from 'dotenv'
+dotenv.config()
 
-const getHomeAssets: RequestHandler = (req, res, next) => {
+const CF_ACCESS_KEY = String(process.env.CF_ACCESS_KEY)
+
+const CF_SECRET_KEY = String(process.env.CF_SECRET_KEY)
+
+const CF_ACCOUNT_ID = String(process.env.CF_ACCOUNT_ID)
+const S3 = new S3Client({
+    region: 'auto',
+    endpoint: `https://${CF_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+    credentials: {
+        accessKeyId: CF_ACCESS_KEY,
+        secretAccessKey: CF_SECRET_KEY
+    }
+})
+
+const getHomeAssets: RequestHandler = async(req, res, next) => {
     try{
-        res.json({msg: "success"})
+        const url = await getSignedUrl(
+            S3,
+            new GetObjectCommand({Bucket: 'hallowedvisions', Key: "angelwingsO.glb"}),
+            {expiresIn: 3600}
+        )
+        res.json({url: url})
     } catch(error) {
         next(error)
     }
