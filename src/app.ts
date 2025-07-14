cae59eea5c523ad365307e665273befa178b7549
HezerSantos <HezerSantos24@gmail.com>
2025-07-14 13:05:05 -0500

    Added the portfolio assets controller and router for portfolio header image

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ import errorMiddleware from './middleware/errors/errorMiddleware'
 import csrfRouter from './routes/csrf/csrfRouter'
 import homeRouter from './routes/home/homeRouter'
 import validateCsrf from './middleware/csrf/validateCsrf'
+import portfolioRouter from './routes/portfolio/portfolioRouter'
 dotenv.config()
 const app = express()
 app.set('trust proxy', 1)
@@ -17,8 +18,8 @@ app.use(bodyParser)
 app.use(cors)
 
 app.use("/api/csrf", csrfRouter)
-app.use("/api/home", validateCsrf,homeRouter)
-
+app.use("/api/home", validateCsrf, homeRouter)
+app.use('/api/portfolio', validateCsrf, portfolioRouter)
 app.use(errorMiddleware)
 const PORT = Number(process.env.PORT) || 8080
 app.listen(PORT, '0.0.0.0', () => {
