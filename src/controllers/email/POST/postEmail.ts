f252c5466cb0bc85ed09f39b16d8061174d9a9c9
HezerSantos <HezerSantos24@gmail.com>
2025-07-21 00:09:37 -0500

    More progress on email

--- src/controllers/email/POST/postEmail.ts
+++ src/controllers/email/POST/postEmail.ts
@@ -1,4 +1,5 @@
 import { RequestHandler } from "express";
+import resend from "../../../services/resend/resend";
 
 interface EmailDetails {
   firstName: string;
@@ -14,7 +15,36 @@ const postEmail: RequestHandler = async(req, res, next) => {
     try{
         const {firstName, lastName, phoneNumber, email, company, websiteType, message} = req.body as EmailDetails
 
-        console.log(firstName)
+        // const res = await resend.emails.send({
+        //     from: `${company? company : "Client"} <client@hallowedvisions.com>`,
+        //     to: "hezernsantos@gmail.com",
+        //     subject: "Client Contact",
+        //     html: `
+        //         <p>
+        //             FROM: ${firstName} ${lastName}
+        //         </p>
+        //         <p>
+        //             Type: ${websiteType? websiteType : "N/A"}
+        //         </p>
+        //         <br/>
+        //         <p>
+        //             ${message}
+        //         </p>
+        //         <br/>
+        //         <p>
+        //             ${phoneNumber}
+        //         </p>
+        //         <p>
+        //             ${email}
+        //         </p>
+        //     `
+        // })
+
+
+        setTimeout(() => {
+            console.log("Client Email Sent")
+            res.status(200).end()
+        }, 5000)
     } catch (error) {
         next(error)
     }
