c59387f440f809033a6dff138c6ca97e07352723
HezerSantos <HezerSantos24@gmail.com>
2025-07-22 23:40:05 -0500

    Added the csrf transform

--- src/controllers/csrf/getCsrf.ts
+++ src/controllers/csrf/getCsrf.ts
@@ -10,7 +10,7 @@ const getCsrfController: RequestHandler = async(req, res, next) => {
     try{
         const csrfValue = crypto.randomBytes(32).toString('hex')
 
-        const csrfToken:string = jwt.sign({csrfToken: csrfValue}, String(process.env.CSRF_SECRET), {expiresIn: '15m'})
+        const csrfToken:string = jwt.sign({csrfToken: csrfValue, key: Math.floor(Math.random() * 10)}, String(process.env.CSRF_SECRET), {expiresIn: '15m'})
         res.cookie('__Secure-auth.csrf', csrfToken, {
             httpOnly: false, 
             secure: true, 
