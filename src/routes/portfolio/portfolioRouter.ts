c6263b6eb9f291336b19598b9d54782a72779de8
HezerSantos <HezerSantos24@gmail.com>
2025-11-30 22:23:14 -0600

    Created the projects controller

--- src/routes/portfolio/portfolioRouter.ts
+++ src/routes/portfolio/portfolioRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import getPortfolioAssets from '../../controllers/portfolio/GET/getPortfolioAssets'
+import getProjectAssets from '../../controllers/portfolio/GET/getProjectAssets'
 
 const portfolioRouter = Router()
 
 portfolioRouter.get("/", getPortfolioAssets)
+portfolioRouter.get("/:id", getProjectAssets)
 
 export default portfolioRouter
