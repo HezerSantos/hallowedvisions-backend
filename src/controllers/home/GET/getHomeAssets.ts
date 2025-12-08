06ee61690f5470e221f64971786167956debc22e
HezerSantos <HezerSantos24@gmail.com>
2025-12-08 16:45:16 -0600

    Added a cache for the 3d asset

--- src/controllers/home/GET/getHomeAssets.ts
+++ src/controllers/home/GET/getHomeAssets.ts
@@ -1,9 +1,25 @@
 import { RequestHandler } from "express";
 import getR2Object from "../../../services/getR2Object";
 
+
+
+interface cacheValue {
+    value: string,
+    expiration: number
+}
+
+const cache = new Map<string, cacheValue>()
 const getHomeAssets: RequestHandler = async(req, res, next) => {
     try{
+        console.log(cache)
+        const cachedValue = cache.get('url');
+        if (cachedValue && cachedValue.expiration > Date.now()) {
+            return res.json({ url: cachedValue.value });
+        }
+
         const url = await getR2Object('hallowedvisions', "angelwingsO.glb")
+
+        cache.set('url', {value: url, expiration: Date.now() + 3600000})
         res.json({url: url})
     } catch(error) {
         next(error)
